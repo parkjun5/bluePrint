5ad361774863699513c1527679509340e415132e
박세준 <sjpark@selab.co.kr>
2022-09-29 14:14:31 +0900

    Initial commit

---
+++ src/main/java/example/blueprint/restapi/BluePrintApplication.java
@@ -0,0 +1,13 @@
+package example.blueprint.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluePrintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluePrintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/blueprint/restapi/BluePrintApplicationTests.java
@@ -0,0 +1,13 @@
+package example.blueprint.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluePrintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
